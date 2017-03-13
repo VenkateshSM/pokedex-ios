aa7626501843dca71be993e46c1cf506ca9ce0f2
Venkatesh SM <venkateshsm@venkateshs-macbook-air.local>
2017-03-13 21:30:26 +0530

    Initial Commit

---
+++ pokedex-by-smv/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-smv
+//
+//  Created by Venkatesh SM on 13/03/17.
+//  Copyright © 2017 Venkatesh SM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
